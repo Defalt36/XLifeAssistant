6a34c1518ad15522aabfe86f0cb8440e2daf642e
Defalt36 <FaelOtk@gmail.com>
2024-01-23 17:53:40 -0300

    Update crossInstallLibrary.sh

--- scripts/crossInstallLibrary.sh
+++ scripts/crossInstallLibrary.sh
@@ -24,6 +24,7 @@ if [ $library = "libz" ] ; then
 	echo
 	echo "Preparing LibZ..."
 	rm -rf zlib-1.3.1
+	#wget https://github.com/madler/zlib/releases/download/v1.3.1/zlib-1.3.1.tar.gz -O- | tar xfz -
 	wget https://zlib.net/zlib-1.3.1.tar.gz -O- | tar xfz -
 	cd zlib-1.3.1
 	sudo make -f win32/Makefile.gcc BINARY_PATH=$prefixdir/bin INCLUDE_PATH=$prefixdir/include LIBRARY_PATH=$prefixdir/lib SHARED_MODE=1 PREFIX=$host- install
@@ -37,6 +38,7 @@ if [ $library = "libpng" ] ; then
 	echo
 	echo "Preparing LibPNG..."
 	rm -rf libpng-1.6.40
+	#wget https://github.com/pnggroup/libpng/archive/refs/tags/v1.6.40.tar.gz -O- | tar xfz -
 	wget https://downloads.sourceforge.net/project/libpng/libpng16/1.6.40/libpng-1.6.40.tar.gz -O- | tar xfz -
 	cd libpng-1.6.40
 	./configure \
