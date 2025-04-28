9409ad2eddbb412d637581f39ef5d5c243a3884b
Defalt36 <FaelOtk@gmail.com>
2025-04-27 23:04:59 -0300

    Update wipeTestServer.sh

--- scripts/wipeTestServer.sh
+++ scripts/wipeTestServer.sh
@@ -2,13 +2,14 @@
 
 TESTDIR=$TESTBUILDPATH
 
-( cd $TESTDIR ; rm -v biome.db eve.db floor.db floorTime.db grave.db lookTime.db map.db mapTime.db meta.db playerStats.db \
-map.db.trunc \
-biomeRandSeed.txt familyDataLog.txt log.txt recentPlacements.txt testMapStale.txt \
+( cd $TESTDIR ; rm -v map.db.trunc \
+biomeRandSeed.txt curseSave.txt kissTest.txt familyDataLog.txt log.txt recentPlacements.txt testMapStale.txt \
 mapDummyRecall.txt shutdownLongLineagePos.txt lastEveLocation.txt )
 
-find $TESTDIR/curseLog -type f -name '*.txt' -exec rm -v {} +
-find $TESTDIR/failureLog -type f -name '*.txt' -exec rm -v {} +
-find $TESTDIR/foodLog -type f -name '*.txt' -exec rm -v {} +
-find $TESTDIR/lifelog -type f -name '*.txt' -exec rm -v {} +
-find $TESTDIR/mapChangeLogs -type f -name '*.txt' -exec rm -v {} +
+cd $TESTDIR
+
+find . -type f -name '*.db' -exec rm -v {} +
+
+for directory in 'curseLog' 'failureLog' 'foodLog' 'lifelog' 'mapChangeLogs' ; do
+    find $directory -maxdepth 1 -type f -name '*.txt' -exec rm -v {} +
+done
